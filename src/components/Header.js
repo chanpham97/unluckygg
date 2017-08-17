87f7385dfde4fc76af7b4c27f6b711d2fba04bef
Chan Pham <tqp2001@columbia.edu>
2017-08-17 03:03:46 +0200

    navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,24 @@ import React, { Component } from 'react';
 
 import '../App.css';
 
-class SearchBar extends Component {
+class Header extends Component {
   render() {
     return (
-      <div className="header">
-        <h1>UNLUCKY gg</h1>
-      </div>
+      <nav className="navbar navbar-inverse">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <a className="navbar-brand" href="#">UNLUCKYgg</a>
+          </div>
+          <ul className="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">Page 1</a></li>
+            <li><a href="#">Page 2</a></li>
+            <li><a href="#">Page 3</a></li>
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
 
-export default SearchBar;
+export default Header;
